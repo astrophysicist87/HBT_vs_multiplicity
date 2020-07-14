ea433c22db2245bd86e5678139c8fb73b13475bc
Christopher Plumberg <astrophysicist87@gmail.com>
2020-07-14 11:07:33 -0400

    Pass all arguments.

--- run_svwr.sh
+++ run_svwr.sh
@@ -15,7 +15,15 @@ cp $workingDirectory/job-1/event-1/surface.dat   $resultsDirectory
 	cd $workingDirectory/HoTCoffeeh
 		
 	nice -n 0 time bash ./HoTCoffeeh.sh true false \
-		SV_resonanceThreshold=0.00 \
+		SV_npphi=48 chosenParticlesMode=0 qtnpts=51 delta_qz=0.0125 \
+		calculate_CF_mode=0 flag_negative_S=1 CF_resonanceThreshold=0.0 \
+		particle_diff_tolerance=0.0 nKT=101 KTmin=0.01 CF_npY=21 KTmax=1.01 \
+		use_plane_psi_order=0 CF_npT=15 fit_with_projected_cfvals=1 \
+		use_log_fit=1 CF_npphi=36 flesh_out_cf=1 use_lambda=1 tolerance=0.0 \
+		qynpts=7 ignore_long_lived_resonances=1 delta_qx=0.025 delta_qy=0.025 \
+		include_bulk_pi=1 grouping_particles=0 SV_resonanceThreshold=0.0 \
+		max_lifetime=100.0 include_delta_f=0 SV_npT=15 use_extrapolation=1 \
+		n_order=4 qxnpts=7 qznpts=7 nKphi=48 \
 		&> ./results/all.out
 
 	mv results ../job-1/event-1/post_processing_results
