d8f1f06af3df6c1fd77db3e28067a72ed7071d44
Christopher Plumberg <plumberg.1@osu.edu>
2020-07-15 14:02:04 +0200

    Added some new scripts to facilitate tuning and submitting of hydro jobs

--- submit_pp_pPb_PbPb_jobs.sh
+++ submit_pp_pPb_PbPb_jobs.sh
@@ -1,25 +1,83 @@
 #! /usr/bin/env bash
+#-------------------
 
-for cen in '0-10%' '10-20%' '20-30%' '30-40%' '40-50%' '50-60%' '60-70%' '70-80%' '80-90%' '90-100%'
-do
-	python updateParameterDict.py ParameterDict_EA_pp_C${cen}_run2.py initial_condition_control:'centrality'=${cen}
-	./generateJobs_local.py 1 10000 PlayGround_EA_pp_C${cen}_run2 RESULTS_EA_pp_C${cen}_run2 03:00:00 no ParameterDict_EA_pp_C${cen}_run2.py
-	./submitJobs_local.py
-	sleep 5
-done
+sys=$1
 
-echo 'Finished all in' `pwd`
-#! /usr/bin/env bash
+case "$sys" in
+	pp)
+            for cen in '0-0.01%' '0-0.1%' '0-1%' '0-10%' '10-20%' '20-30%' '30-40%' '40-50%' '50-60%' '60-70%' '70-80%' '80-90%' '90-100%' '0-100%'
+            do
+                python updateParameterDict.py ParameterDict_EA_${sys}_C${cen}.py \
+                        initial_condition_control:'centrality'=${cen} \
+                        superMCParameters:'Aproj'=1 \
+                        superMCParameters:'Atarg'=1 \
+                        superMCParameters:'ecm'=7000 \
+                        superMCParameters:'finalFactor'=81.1114
+                        
+                ./generateJobs_local.py 1 10000 \
+                    PlayGround_EA_${sys}_C${cen} \
+                    RESULTS_EA_${sys}_C${cen} \
+                    03:00:00 no \
+                    ParameterDict_EA_${sys}_C${cen}.py
+                    
+                ./submitJobs_local.py
+                
+                sleep 5
+            done
+            ;;
+	pPb)
+            for cen in '0-1%' '0-10%' '10-20%' '20-30%' '30-40%' '40-50%' '50-60%' '60-70%' '70-80%' '80-90%' '90-100%' '0-100%'
+            do
+                python updateParameterDict.py ParameterDict_EA_${sys}_C${cen}.py \
+                       initial_condition_control:'centrality'=${cen} \
+                        superMCParameters:'Aproj'=1 \
+                        superMCParameters:'Atarg'=208 \
+                        superMCParameters:'ecm'=5020 \
+                        superMCParameters:'finalFactor'=52.663
+                       
+                ./generateJobs_local.py 1 10000 \
+                  PlayGround_EA_${sys}_C${cen} \
+                  RESULTS_EA_${sys}_C${cen} \
+                  03:00:00 no \
+                  ParameterDict_EA_${sys}_C${cen}.py
+                  
+                ./submitJobs_local.py
+                
+                sleep 5
+            done
+            ;;
 
-for cen in '0-100%' '0-1%' '0-10%' '10-20%' '20-30%' '30-40%' '40-50%' '50-60%' '60-70%' '70-80%' '80-90%' '90-100%'
-do
-	python updateParameterDict.py ParameterDict_EA_pPb_C${cen}.py initial_condition_control:'centrality'=${cen}
-	./generateJobs_local.py 1 10000 PlayGround_EA_pPb_C${cen} RESULTS_EA_pPb_C${cen} 03:00:00 no ParameterDict_EA_pPb_C${cen}.py
-	./submitJobs_local.py
-	sleep 5
-done
+	PbPb)
+            for cen in '0-10%' '10-20%' '20-30%' '30-40%' '40-50%' '50-60%' '60-70%' '70-80%' '80-90%' '90-100%' '0-100%'
+            do
+                python updateParameterDict.py ParameterDict_EA_${sys}_C${cen}.py \
+                       initial_condition_control:'centrality'=${cen} \
+                        superMCParameters:'Aproj'=208 \
+                        superMCParameters:'Atarg'=208 \
+                        superMCParameters:'ecm'=2760 \
+                        superMCParameters:'finalFactor'=55.3037
+                       
+                ./generateJobs_local.py 1 10000 \
+                  PlayGround_EA_${sys}_C${cen} \
+                  RESULTS_EA_${sys}_C${cen} \
+                  24:00:00 no \
+                  ParameterDict_EA_${sys}_C${cen}.py
+                  
+                ./submitJobs_local.py
+                
+                sleep 5
+            done
+            ;;
 
-echo 'Finished all in' `pwd`
+	*)
+            echo $"Usage: $0 {pp|pPb|PbPb}"
+            exit 1
+ 
+esac
+
+
+
+
 #! /usr/bin/env bash
 
 # Tune "finalfactor" first
@@ -32,12 +90,5 @@ echo 'Finished all in' `pwd`
 #done
 #finalFactor obtained: 55.3037
 
-for cen in '0-100%' '0-10%' '10-20%' '20-30%' '30-40%' '40-50%' '50-60%' '60-70%' '70-80%' '80-90%' '90-100%'
-do
-        python updateParameterDict.py ParameterDict_EA_PbPb_C${cen}.py initial_condition_control:'centrality'=${cen}
-        ./generateJobs_local.py 1 10000 PlayGround_EA_PbPb_C${cen} RESULTS_EA_PbPb_C${cen} 24:00:00 no ParameterDict_EA_PbPb_C${cen}.py
-        ./submitJobs_local.py
-	sleep 5
-done
 
-echo 'Finished all in' `pwd`
+
---
+++ tune_pp_pPb_PbPb_jobs.sh
@@ -0,0 +1,85 @@
+#! /usr/bin/env bash
+#-------------------
+
+sys=$1
+
+case "$sys" in
+	pp)
+	    cen='0-100%'
+	    #Target dNchdeta: 6.0
+            for ff in 70 75 76 77 78 79 80 81 82 83 84 85 90
+            do
+                python updateParameterDict.py ParameterDict_EA_${sys}_C${cen}_ff${ff}.py \
+                        initial_condition_control:'centrality'=${cen} \
+                        superMCParameters:'Aproj'=1 \
+                        superMCParameters:'Atarg'=1 \
+                        superMCParameters:'ecm'=7000 \
+                        superMCParameters:'finalFactor'=${ff} \
+                        HoTCoffeehControl:'runHoTCoffeeh'=False
+                        
+                ./generateJobs_local.py 1 10000 \
+                  PlayGround_EA_${sys}_C${cen}_ff${ff} \
+                  RESULTS_EA_${sys}_C${cen}_ff${ff} \
+                  03:00:00 no \
+                  ParameterDict_EA_${sys}_C${cen}_ff${ff}.py
+                    
+                ./submitJobs_local.py
+                
+                sleep 5
+            done
+            ;;
+	pPb)
+	    cen='0-5%'
+	    #Target dNchdeta: 17.5
+            for ff in 35 40 45 50 53 54 55 56 57 60 65 70 75 80
+            do
+                python updateParameterDict.py ParameterDict_EA_${sys}_C${cen}_ff${ff}.py \
+                       initial_condition_control:'centrality'=${cen} \
+                        superMCParameters:'Aproj'=1 \
+                        superMCParameters:'Atarg'=208 \
+                        superMCParameters:'ecm'=5020 \
+                        superMCParameters:'finalFactor'=${ff} \
+                        HoTCoffeehControl:'runHoTCoffeeh'=False
+                       
+                ./generateJobs_local.py 1 10000 \
+                  PlayGround_EA_${sys}_C${cen}_ff${ff} \
+                  RESULTS_EA_${sys}_C${cen}_ff${ff} \
+                  03:00:00 no \
+                  ParameterDict_EA_${sys}_C${cen}_ff${ff}.py
+                  
+                ./submitJobs_local.py
+                
+                sleep 5
+            done
+            ;;
+
+	PbPb)
+	    cen='0-5%'
+	    #Target dNchdeta: 1601
+            for ff in 35 40 45 50 53 54 55 56 57 60 65 70 75 80
+            do
+                python updateParameterDict.py ParameterDict_EA_${sys}_C${cen}_ff${ff}.py \
+                       initial_condition_control:'centrality'=${cen} \
+                        superMCParameters:'Aproj'=208 \
+                        superMCParameters:'Atarg'=208 \
+                        superMCParameters:'ecm'=2760 \
+                        superMCParameters:'finalFactor'=${ff} \
+                        HoTCoffeehControl:'runHoTCoffeeh'=False
+                       
+                ./generateJobs_local.py 1 10000 \
+                  PlayGround_EA_${sys}_C${cen}_ff${ff} \
+                  RESULTS_EA_${sys}_C${cen}_ff${ff} \
+                  03:00:00 no \
+                  ParameterDict_EA_${sys}_C${cen}_ff${ff}.py
+                  
+                ./submitJobs_local.py
+                
+                sleep 5
+            done
+            ;;
+
+	*)
+            echo $"Usage: $0 {pp|pPb|PbPb}"
+            exit 1
+ 
+esac
