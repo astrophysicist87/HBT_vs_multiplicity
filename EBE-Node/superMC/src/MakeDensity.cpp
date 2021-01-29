b2fb678dc67d39ef317af891f7a18ea0c40442f6
Christopher Plumberg <plumberg.1@osu.edu>
2021-01-29 12:19:57 -0600

    Update MakeDensity.cpp

--- EBE-Node/superMC/src/MakeDensity.cpp
+++ EBE-Node/superMC/src/MakeDensity.cpp
@@ -157,8 +157,8 @@ MakeDensity::~MakeDensity()
 void MakeDensity::generate_profile_ebe_Jet(int nevent)
 {
   // binary profile:
-  const char resultsPath_CString[] = resultsPath.c_str();
-  const char file_binary[] = {resultsPath_CString"/BinaryCollisionTable_event_%d.dat"};
+  const char * resultsPath_CString = resultsPath.c_str();
+  const char file_binary[] = (resultsPath_CString"/BinaryCollisionTable_event_%d.dat");
   char file_participant[] = (resultsPath + "/ParticipantTable_event_%d.dat").c_str();
 
   // entropy profile:
